48c39dbca2884481f6e7613d5e242b62e0a9ec67
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-10-04 15:42:46 -0700

    Todos added successfully on init

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos;
 
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+      text: 'Pickup kids from school.'
+      },
+      {
+        text: 'Meeting with boss.'
+      },
+      {
+        text: 'Code, duh.'
+      }
+  ]
   }
 
 }
