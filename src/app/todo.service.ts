3c1f0d53d968f738f83ee078702c2ca8b3b38b32
Brittany Lacoste <lacostebrittany0227@gmail.com>
2017-10-05 11:28:28 -0700

    Extend class init todo sample list

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Init } from './init-todos';
 
 @Injectable()
-export class TodoService {
+export class TodoService extends Init {
 
   constructor() { 
+    super();
     console.log('TodoService Initialized');
+    this.load();
   }
 
   getTodos() {
